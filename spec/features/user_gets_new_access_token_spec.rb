10378ff9e2a70a3b83f35aad81fc65af38d5d4f3
luismenjivar <luismenjivr@gmail.com>
2015-05-12 22:20:26 -0400

    changed access token to api key

--- spec/features/user_gets_new_access_token_spec.rb
+++ spec/features/user_gets_new_access_token_spec.rb
@@ -11,7 +11,7 @@ feature 'user gets new access token' do
   scenario 'for the first time' do
     expect(@user.apikey).to eq(nil)
     visit edit_user_registration_path
-    click_button 'Get An Access Token'
+    click_button 'Get An Api Key'
     expect(page).to have_content('You have succesfully created api key')
     expect(@user.reload.apikey.access_key).not_to eq(nil)
   end
@@ -19,9 +19,9 @@ feature 'user gets new access token' do
   scenario 'changes access token' do
     expect(@user.apikey).to eq(nil)
     visit edit_user_registration_path
-    click_button 'Get An Access Token'
+    click_button 'Get An Api Key'
     old_access_key = @user.reload.apikey.access_key
-    click_button 'Change your Access Token'
+    click_button 'Change Your Api Key'
     expect(page).to have_content('You have changed your api key')
     expect(@user.apikey).not_to eq(old_access_key)
   end
